8f6361f7d20601c229b7ba73fb88a11ba495c231
nfenampainfe <nfenampai@gmail.com>
2026-09-10 14:59:38 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -1513,7 +1513,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbzh6OENPRst0ehDHg83BZlSWp85jN2tYBXyk3y5gISlITOFeW3oi2yVHErvLgXOiRB9/exec';
 
 ;
 
