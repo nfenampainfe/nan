a0177e42e5800f7229372ad2e7184d77c5fdfd25
nfenampainfe <nfenampai@gmail.com>
2026-09-10 14:57:20 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: ''
+    API_URL: 'https://script.google.com/macros/s/AKfycbzIoXLeT-mOjs0cRH4ofCnPVpJjWv7lVm7pqm957SNkZQh-L8c1uVqOsoNaxXUAxUBU/exec'
   });
 })();
